2d621412cfc8dce314dfdc47f599ddd8f2d23c26
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-01-03 12:18:07 +0100

    test: :white_check_mark: Updated e2e tests for login

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,5 +1,44 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  it('Should verify that page exists', () => {
+    cy.visit('/login')
+
+    cy.get('.mat-card-title')
+      .should('exist')
+      .and('contain.text', 'Login')
+  })
+
+  it('Should have an empty input error', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').focus().blur()
+    cy.get('input[formControlName=password]').focus()
+
+    cy.get('input[formControlName=email]')
+      .should('have.class', 'ng-invalid')
+  })
+
+  it('Should toggle password visibility', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=password]')
+      .should('have.attr', 'type', 'password')
+      .type('test123')
+
+    cy.get('button[data-testid=toggle-pass-button]').click()
+
+    cy.get('input[formControlName=password]')
+      .should('have.attr', 'type', 'text')
+  })
+
+  it('Should enable submit button', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type('test123')
+    cy.get('button[data-testid=submit-button]').should('not.be.disabled')
+  })
+
+  it('Should get successfull login', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
@@ -23,5 +62,23 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  })
+
+  it('Should get unsuccessfull login', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Bad credentials'
+      }
+    }).as('login')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!123"}{enter}{enter}`)
+
+    cy.get('.error')
+      .should('exist')
+      .and('contain.text', 'An error occurred')
   })
 });
